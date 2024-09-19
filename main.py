d70663e205a425331ea784eb2b7f0484c27c3c3a
petrtatar <tatar.petr523@gmail.com>
2024-09-19 06:04:39 +0300

    Update main.py
    Added to main.py code for import variables from .env

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 
+import asyncio
+import logging
+
+import aiogram
+from aiogram import Bot, Dispatcher, F
+import os
+from dotenv import load_dotenv
+from handlers import other_handlers, user
+
+load_dotenv()
+
+bot_token = os.getenv('BOT_TOKEN')
+bot = Bot(bot_token)
+
+dp = Dispatcher()
+
+dp.include_router(user.router)
+dp.include_router(other_handlers.router)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        dp.run_polling(bot)
+    except KeyboardInterrupt:
+        print('Exit')
